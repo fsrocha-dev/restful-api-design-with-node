f4494e814d98e5d3bc8c0b6df2a1260a894af24a
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-19 11:34:10 -0300

    configure url parse to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
-require("dotenv/config");
 
 module.exports = app
--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose')
+require("dotenv/config");
 
-mongoose.connect(process.env.DB_CONNECTION)
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
